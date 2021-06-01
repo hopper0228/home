1870604385c7f2e3655f21ac6829726f6e415ee8
lail26266@gmail.com <lail26266@gmail.com>
2021-06-01 16:06:09 +0800

    0601

---
+++ src/main/java/com/tom/car/CarDemo.java
@@ -0,0 +1,18 @@
+package com.tom.car;
+
+import javax.swing.*;
+
+public class CarDemo {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JButton button1;
+    private JPanel northPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setContentPane(new CarDemo().mainPanel);
+        frame.setSize(600,400);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
